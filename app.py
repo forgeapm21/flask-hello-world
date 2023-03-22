86f6eef3999dfc058be6771456b1e44a6f7be346
forgeapm21 <128446805+forgeapm21@users.noreply.github.com>
2023-03-22 02:32:12 +0000

    love

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask import escape
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    print ('FBGM')
-    return 'Hello, World!'
+    return 'FBGM'
+
+@app.route("/<lover>")
+def who_do_ya_love(lover):
+    if escape(lover) == "caitlyn":
+        return f"I love {escape(lover)}!"
+    else:
+        return "That's not my lover!"
